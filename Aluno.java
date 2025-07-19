9187db3c3749478b0a044d8c33f1124f729e2920
Nicolas Gomes <nico014932@gmail.com>
2025-07-19 14:16:47 -0300

    Criação dos atributos

--- Aluno.java
+++ Aluno.java
@@ -1,3 +1,11 @@
 public class Aluno {
+    private String matricula;
+    private String nome;
+    private String cpf;
+    private float dataNascimento;
+    private String Sexo;
+    private String endereco;
+    private String cidade;
+    private String uf;
     
 }
