0fa93f1a51e85a8756f260148145bd13c08e6081
Nicolas Gomes <nico014932@gmail.com>
2025-07-19 14:17:55 -0300

    Criação dos getters e setters

--- Aluno.java
+++ Aluno.java
@@ -8,4 +8,53 @@ public class Aluno {
     private String cidade;
     private String uf;
     
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public float getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(float dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    public String getSexo() {
+        return Sexo;
+    }
+    public void setSexo(String sexo) {
+        Sexo = sexo;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getUf() {
+        return uf;
+    }
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    
 }
