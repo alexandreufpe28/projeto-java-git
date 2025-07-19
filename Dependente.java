52d3a01eea2993ead3f13c47c39d2f4a72a703f0
Deivid Marques <deividmarqs@gmail.com>
2025-07-19 14:30:49 -0300

    criação dos atributos

--- Dependente.java
+++ Dependente.java
@@ -1,4 +1,12 @@
+import java.time.LocalDate;
+
 public class Dependente {
-    
+
+    private int sequencial;
+    private String nomeCompleto;
+    private LocalDate dataNascimento;
+    private Integer sexo;
+    private boolean participaPlano;
+
 
 }
