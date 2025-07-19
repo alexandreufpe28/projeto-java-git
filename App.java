33e94ac3dfe264edc592272646ebbdc0547d6490
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-07-19 13:45:32 -0300

    Correção do código de Rafael

--- App.java
+++ App.java
@@ -7,9 +7,9 @@ public class App {
 
         System.out.println("Digite seu nome:");
         String nome = sc.nextLine();
-        System.out.println("Digite sua idade:");
-        String idade = sc.nextInt();
 
+        System.out.println("Digite sua idade:");
+        int idade = sc.nextInt();
 
         sc.close();
 
