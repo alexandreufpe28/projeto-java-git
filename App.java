fb104d742fc2fc53d7ecbd0d9daa689e10851e0e
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-07-19 13:39:29 -0300

    Solicitação do nome do usuário

--- App.java
+++ App.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        String nome = sc.nextLine();
+
+        sc.close();
+
+        System.out.println("Olá, " + nome + "!");
     }
     
 }
