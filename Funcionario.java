11a3b76e7ae4376d7d27852375b6f3c5163825ea
Priscylla Rocha <priscyllarocha104@gmail.com>
2025-07-19 14:06:19 -0300

    Adição dos atributos à classe Funcionário

--- Funcionario.java
+++ Funcionario.java
@@ -1,3 +1,16 @@
+import java.time.LocalDate;
+
 public class Funcionario {
+    
+    private String matricula;
+    private String nomeCompleto;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private char sexo;
+    private double salario;
+    private int telefone;
+
+    
+
 
 }
